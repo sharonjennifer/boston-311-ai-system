1d0e9f87cb366d5c92ef57c06b41af5b34f843ce
ProfSchrodinger <sabarimathavan24@gmail.com>
2025-12-04 23:52:42 -0500

    moved ecplicit values to .env file

--- chatbot/sql_model/chatbot_endpoint_test.py
+++ chatbot/sql_model/chatbot_endpoint_test.py
@@ -1,11 +1,25 @@
 import json
+import logging
+import os
+
+from dotenv import load_dotenv
 from google.cloud import aiplatform_v1
 from google.api_core.client_options import ClientOptions
 from google.api import httpbody_pb2
 
-PROJECT_ID = "boston311-mlops"
-REGION = "us-central1"
-ENDPOINT_ID = "8571536463932424192"
+load_dotenv()
+
+PROJECT_ID = os.getenv("B311_PROJECT_ID")
+REGION = os.getenv("B311_VERTEX_LOCATION", "us-central1")
+LOG_LEVEL = os.getenv("B311_LOG_LEVEL", "INFO").upper()
+ENDPOINT_ID = os.getenv("B311_SQLCODER_ENDPOINT_ID")
+
+logging.basicConfig(
+    level=os.getenv("B311_LOG_LEVEL", "INFO"),
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+logger = logging.getLogger("b311.endpoint_test")
 
 
 
@@ -17,6 +31,7 @@ endpoint_path = client.endpoint_path(
     project=PROJECT_ID, location=REGION, endpoint=ENDPOINT_ID
 )
 
+logger.info("Endpoint path: %s", endpoint_path)
 prompt = """### Task
 Generate a BigQuery SQL query to answer [QUESTION]which hour of the day sees the most parking tickets[/QUESTION]
 
@@ -41,6 +56,7 @@ http_body = httpbody_pb2.HttpBody(
     data=json.dumps(request_body).encode("utf-8"),
 )
 
+logger.info("Sending request to Vertex AI…")
 response = client.raw_predict(
     endpoint=endpoint_path,
     http_body=http_body,
